1038db792979d6fdd6fa2471bdbbd9d85b354081
biccon <admin@idx.kr>
2019-12-06 01:45:50 +0900

    table 최신화

--- 쿼리/tables.sql
+++ 쿼리/tables.sql
@@ -1,54 +1,72 @@
 create database library default CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 use library;
 
-create table user(
-   user_no int primary key auto_increment,
-   user_id varchar(32) not null,
-   user_password varchar(32) not null,
-   user_name varchar(32) not null,
-   phonenum varchar(12),
-   email varchar(32),
-   max int
-);
+CREATE TABLE `area` (
+  `area_no` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(32) NOT NULL,
+  PRIMARY KEY (`area_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table area(
-   area_no int primary key auto_increment,
-   category varchar(32) not null
-);
+CREATE TABLE `book` (
+  `book_unique_no` int(11) NOT NULL AUTO_INCREMENT,
+  `book_no` int(11) DEFAULT NULL,
+  `condition` varchar(10) NOT NULL,
+  PRIMARY KEY (`book_unique_no`),
+  FOREIGN KEY (`book_no`) REFERENCES `book_detail` (`book_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table book_detail(
-   book_no int primary key auto_increment,
-   area_no int,
-   title varchar(32) not null,
-   subtitle varchar(32) not null,
-   publisher varchar(32) not null,
-   published_date datetime,
-   author varchar(32) not null, -- 저자가 여러명인 경우 어떻게 할까? 저자테이블 추가하거나, 그냥 놔두거나
-   foreign key (area_no) references area(area_no)
-);
+CREATE TABLE `book_detail` (
+  `book_no` int(11) NOT NULL AUTO_INCREMENT,
+  `area_no` int(11) DEFAULT NULL,
+  `title` varchar(50) NOT NULL,
+  `publisher` varchar(32) NOT NULL,
+  `published_date` datetime DEFAULT NULL,
+  `author` varchar(32) NOT NULL,
+  PRIMARY KEY (`book_no`),
+  FOREIGN KEY (`area_no`) REFERENCES `area` (`area_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table book (
-   book_unique_no varchar(32) primary key,
-   book_no int,
-   `condition` varchar(10) not null,
-   foreign key (book_no) references book_detail(book_no)
-);
+CREATE TABLE `rental` (
+  `rental_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_no` int(11) DEFAULT NULL,
+  `book_unique_no` int(11) DEFAULT NULL,
+  `rental_date` datetime DEFAULT NULL,
+  `due_date` datetime DEFAULT NULL,
+  `return_date` datetime DEFAULT NULL,
+  `note` varchar(50) NOT NULL,
+  PRIMARY KEY (`rental_no`),
+  FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`),
+  FOREIGN KEY (`book_unique_no`) REFERENCES `book` (`book_unique_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table rental(
-   user_no int,
-   book_no int,
-   rental_date datetime default CURRENT_TIMESTAMP,
-   return_date datetime,
-   state varchar(10) not null,
-   foreign key(user_no) references user(user_no),
-   foreign key(book_no) references book_detail(book_no)
-   );
+CREATE TABLE `request` (
+  `request_no` int(11) NOT NULL,
+  `user_no` int(11) DEFAULT NULL,
+  `title` varchar(50) NOT NULL,
+  `publisher` varchar(32) NOT NULL,
+  `author` varchar(32) NOT NULL,
+  `request_date` datetime DEFAULT NULL,
+  `state` varchar(10) NOT NULL,
+  PRIMARY KEY (`request_no`),
+  FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table reservation(
-   user_no int,
-   book_no int,
-   reservation_date datetime,
-   foreign key(user_no) references user(user_no),
-   foreign key(book_no) references book_detail(book_no)
-);
+CREATE TABLE `reservation` (
+  `reservation_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_no` int(11) DEFAULT NULL,
+  `book_no` int(11) DEFAULT NULL,
+  `reservation_date` datetime DEFAULT NULL,
+  `state` varchar(10) NOT NULL,
+  PRIMARY KEY (`reservation_no`),
+  FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`),
+  FOREIGN KEY (`book_no`) REFERENCES `book_detail` (`book_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `user` (
+  `user_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(32) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `phonenum` varchar(12) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`user_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
