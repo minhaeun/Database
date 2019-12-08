56b1baeb27ed01ab35163a6f973e37edba870354
biccon <admin@idx.kr>
2019-12-09 02:12:08 +0900

    .

--- 쿼리/tables.sql
+++ 쿼리/tables.sql
@@ -1,18 +1,19 @@
 create database library default CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 use library;
 
+CREATE TABLE `user` (
+  `user_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(32) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `phonenum` varchar(12) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`user_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `area` (
   `area_no` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(32) NOT NULL,
   PRIMARY KEY (`area_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `book` (
-  `book_unique_no` int(11) NOT NULL AUTO_INCREMENT,
-  `book_no` int(11) DEFAULT NULL,
-  `condition` varchar(10) NOT NULL,
-  PRIMARY KEY (`book_unique_no`),
-  FOREIGN KEY (`book_no`) REFERENCES `book_detail` (`book_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `book_detail` (
@@ -24,6 +25,14 @@ CREATE TABLE `book_detail` (
   `author` varchar(32) NOT NULL,
   PRIMARY KEY (`book_no`),
   FOREIGN KEY (`area_no`) REFERENCES `area` (`area_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `book` (
+  `book_unique_no` int(11) NOT NULL AUTO_INCREMENT,
+  `book_no` int(11) DEFAULT NULL,
+  `condition` varchar(10) NOT NULL,
+  PRIMARY KEY (`book_unique_no`),
+  FOREIGN KEY (`book_no`) REFERENCES `book_detail` (`book_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `rental` (
@@ -60,13 +69,4 @@ CREATE TABLE `reservation` (
   PRIMARY KEY (`reservation_no`),
   FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`),
   FOREIGN KEY (`book_no`) REFERENCES `book_detail` (`book_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `user` (
-  `user_no` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(32) NOT NULL,
-  `user_name` varchar(32) NOT NULL,
-  `phonenum` varchar(12) DEFAULT NULL,
-  `email` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`user_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
